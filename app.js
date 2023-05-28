63691c017efbd1cc3960d4138c7126002966fa7e
rellasie <86995253+rellasie@users.noreply.github.com>
2023-05-28 16:40:48 +0700

    add repeat function

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ const progress = $('#progress')
 const prevBtn = $('.btn-prev')
 const nextBtn = $('.btn-next')
 const randomBtn = $('.btn-random')
+const repeatBtn = $('.btn-repeat')
 
 const app = {
     currentIndex: 0, // current index
     isPlaying: false,
     isRandom: false,
+    isRepeat: false,
 
     songs: [
         {
@@ -169,6 +171,21 @@ const app = {
         randomBtn.onclick = function(e) {
             _this.isRandom = !_this.isRandom
             randomBtn.classList.toggle('active', _this.isRandom)
+        }
+
+        // xử lý lặp lại một song
+        repeatBtn.onclick = function(e) {
+            _this.isRepeat = !_this.isRepeat
+            repeatBtn.classList.toggle('active', _this.isRepeat)
+        }
+
+        // xử lý next song khi audio ended
+        audio.onended = function() {
+            if (_this.isRepeat) {
+                audio.play()
+            } else {
+                nextBtn.click()
+            }
         }
     },
 
